8286556c88db2eb3fcd12e102a31d118f384c0c8
aynaz <aynaz@arianalabs.io>
2023-07-18 16:37:12 +0330

    change theme dark & light

---
+++ app/Provider.jsx
@@ -0,0 +1,13 @@
+'use client'
+import { ThemeProvider } from 'next-themes'
+import React from 'react'
+
+export default function Provider({ children }) {
+    return (
+        <ThemeProvider enableSystem={true} attribute='class'>
+            <div className='transition-colors duration-300 min-h-screen select-none'>
+                {children}
+            </div>
+        </ThemeProvider>
+    )
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MenuItems from './MenuItems'
 import { HiHome } from 'react-icons/hi';
 import { RiInformationFill } from 'react-icons/ri';
-import { BsFillSunFill } from 'react-icons/bs';
+import DarkModeSwitcher from './DarkModeSwitcher';
 
 export default function Header() {
   return (
@@ -12,7 +12,7 @@ export default function Header() {
         <MenuItems title="About" url="/" Icon={RiInformationFill} />
       </div>
       <div className='flex items-center space-x-4 justify-between text-2xl'>
-        <BsFillSunFill className=' hover:text-[#F5C518] hover:cursor-pointer'/>
+        <DarkModeSwitcher />
         <button className='bg-[#F5C518] text-black font-bold rounded-md p-2'>IMDB</button>
         <p className='font-bold'>Clone</p>
       </div>
