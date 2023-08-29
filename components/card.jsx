4721691c2f4dacc13fd72caabad4316bc69e4f82
aynaz <aynaz@arianalabs.io>
2023-08-29 10:12:41 +0330

    FIX : remove warning

--- components/card.jsx
+++ components/card.jsx
@@ -15,7 +15,7 @@ export default function Card({ item, index }) {
           //   item.backdrop_path || item.poster_path
           // }`}
           src={imageLoader}
-          alt={item.title}
+          alt="test image"
           width={500}
           height={300}
           style={{ maxWidth: "100%", height: "auto" }}
