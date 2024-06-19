3ecf37c41f96357f51569353fc677716eb168f4a
aynaz <aynaz@arianalabs.io>
2024-06-19 16:09:17 +0330

    FIX : issue on build

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,9 +8,9 @@ export default function About() {
         video games, and streaming content online – including cast, production
         crew and personal biographies, plot summaries, trivia, ratings, and fan
         and critical reviews. IMDb began as a fan-operated movie database on the
-        Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993.
+        Usenet group rec.arts.movies in 1990, and moved to the Web in 1993.
         Since 1998, it has been owned and operated by IMDb.com, Inc., a
-        subsidiary of Amazon. The site's message boards were disabled in
+        subsidiary of Amazon. The sites message boards were disabled in
         February 2017. As of 2019, IMDb was the 52nd most visited website on the
         internet, as ranked by Alexa. As of March 2022, the database contained
         some 10.1 million titles (including television episodes), 11.5 million
--- app/loading.jsx
+++ app/loading.jsx
@@ -1,12 +1,14 @@
 // import "../public/assets/css/spinner.css";
 
+import Image from "next/image";
+
 export default function Loading() {
   return (
     <div className="flex justify-center items-center w-full my-11">
       {/* <div className="ldio-tfiaujkqh2">
         <div></div>
       </div> */}
-      <img src="/assets/images/spinner.svg" alt="loading ..."/>
+      <Image src="/assets/images/spinner.svg" alt="loading ..."/>
     </div>
   );
 }
